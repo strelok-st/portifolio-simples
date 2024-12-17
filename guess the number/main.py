7d8a165ebbc1300600643e22f0ec3b7b3cecabe2
Patrick <patrick.contato.trabalho@gmail.com>
2024-12-17 10:35:54 -0300

    Terminei a parte lógica do programa.
    Me preparando para fazer o desenvolvimento.

--- guess the number/main.py
+++ guess the number/main.py
@@ -1,101 +1,47 @@
-# importando os módulos
-import random;
+# importando as bibliotecas
+import random
 
-# definindo as variáveis
-global x;
-global y;
-global d;
+def avalie_numero(x):
+    while True:
+        try:
+            y = int(input("Digite um número (ou 'q' para sair): "))
+        except ValueError:
+            print("Entrada inválida. Por favor, digite um número inteiro.")
+            continue
 
-# definindo as funções
-def menu():
-	menu = input(print(
-		"""
-		Oi, eu sou o jogo "Adivinhe o número". Selecione uma das opções abaixo digitando o número e pressionando "Enter":
-		1) Comece o jogo
-		2) Sair
-		"""
-		));
-	if menu == "1":
-		comece_jogo();
-	else:
-		quit();
+        if x == y:
+            print(f"Parabéns, {x} é o número correto!")
+            break  # Sai do loop
+        
+                d = abs(x - y)
 
+        if d <= 10:
+            print("Pelando! Quer tentar de novo?")
+        elif d <= 20:
+            print("Muito quente! Quer tentar de novo?")
+        elif d <= 30:
+            print("Bem quente! Quer tentar de novo?")
+        elif d <= 40:
+            print("Ficando quente! Quer tentar de novo?")
+        elif d <= 50:
+            print("Morno! Quer tentar de novo?")
+        elif d <= 60:
+            print("Ficando gelado! Quer tentar de novo?")
+        elif d <= 70:
+            print("Gelado! Quer tentar de novo?")
+        elif d <= 80:
+            print("Frio! Quer tentar de novo?")
+        else:
+            print("Muito frio! Quer tentar de novo?")
 
-def gere_numero():
-	x = random.randint(0, 100);
-	return x;
-
-def comece_jogo():
-	gere_numero();
-	avalie_numero();
-
-def avalie_numero(x, y, d):
-	while True:
-		y = input("Digite um número: ");
-		y = int(y);
-		if x > y:
-			d = x - y;
-			if 1 <= d <= 10:
-				print("Pelando! Quer tentar de novo?");
-				avalie_numero(x, y, d);
-			elif 11 <= d <= 20:
-				print("Muito quente! Quer tentar de novo?");
-				avalie_numero(x, y, d);
-			elif 21 <= d <= 30:
-				print("Bem quente! Quer tentar de novo?");
-				avalie_numero(x, y, d);
-			elif 31 <= d <= 40:
-				print("Ficando quente! Quer tentar de novo?");
-				avalie_numero(x, y, d);
-			elif 41 <= d <= 50:
-				print("Morno! Quer tentar de novo?");
-				avalie_numero(x, y, d);
-			elif 51 <= d <= 60:
-				print("Ficando gelado! Quer tentar de novo?")
-				avalie_numero(x, y, d);
-			elif 61 <= d <= 70:
-				print("Gelado! Quer tentar de novo?");
-				avalie_numero(x, y, d);
-			elif 71 <= d <= 80:
-				print("Frio! Quer tentar de novo?")
-				avalie_numero(x, y, d);
-			elif 81 <= d <= 90:
-				print("Muito frio! Quer tentar de novo?")
-				avalie_numero(x, y, d);
-		elif x == y:
-			d = x;
-			print(f"Parabéns, {d} é o número correto! ");
-			menu();
-		elif x < y:
-			d = y - x;
-			if 1 <= d <= 10:
-				print("Pelando! Quer tentar de novo?");
-				avalie_numero(x, y, d);
-			elif 11 <= d <= 20:
-				print("Muito quente! Quer tentar de novo?");
-				avalie_numero(x, y, d);
-			elif 21 <= d <= 30:
-				print("Bem quente! Quer tentar de novo?");
-				avalie_numero(x, y, d);
-			elif 31 <= d <= 40:
-				print("Ficando quente! Quer tentar de novo?");
-				avalie_numero(x, y, d);
-			elif 41 <= d <= 50:
-				print("Morno! Quer tentar de novo?");
-				avalie_numero(x, y, d);
-			elif 51 <= d <= 60:
-				print("Ficando gelado! Quer tentar de novo?")
-				avalie_numero(x, y, d);
-			elif 61 <= d <= 70:
-				print("Gelado! Quer tentar de novo?");
-				avalie_numero(x, y, d);
-			elif 71 <= d <= 80:
-				print("Frio! Quer tentar de novo?")
-				avalie_numero(x, y, d);
-			elif 81 <= d <= 90:
-				print("Muito frio! Quer tentar de novo?")
-				avalie_numero(x, y, d);
-
-menu(); # começa o programa
-
-# final do programa;
+# gerando o número da vez;
+while True: 
+	lista = range(1,101);
+	x = random.choice(lista);
+	def menu():
+		escolha = input("1) começa o jogo, 2) sai do jogo")
+		if escolha == "1":
+			avalie_numero(x);
+		else:
+			quit();
+	menu();
--- guess the number/readme.md
+++ guess the number/readme.md
@@ -6,11 +6,11 @@ Um joguinho simples onde o usuário digita números para tentar acertar o valor 
 1. Pesquise como fazer a arquitetura de um software e ir do início ao fim com o projeto. // feito;
 2. Sketch do design do sistema por inteiro (definir o que o sistema precisa fazer, como ele deve se comportar e do que ele precisaria); // feito;
 3. Pesquisar por referências de projetos parecidos se já existem sobre o necessário do programa;
-4. Escreva a parte lógica do programa
-    a. Defina as ações, partes e algoritmo geral do programa;
-    b. Defina as funções do programa;
-    c. Escreva os algoritmos das funções;
-    d. Faça testes para garantir que tudo está correto;
+4. Escreva a parte lógica do programa // feito
+    a. Defina as ações, partes e algoritmo geral do programa; feito;
+    b. Defina as funções do programa; feito;
+    c. Escreva os algoritmos das funções; feito;
+    d. Faça testes para garantir que tudo está correto; feito;
 5. Sketch do design visual da aplicação;
 6. Pesquise por referências de projetos parecidos se já existem sobre o necessário da parte visual da aplicação.
 7. Escreva a parte visual do programa.
@@ -18,5 +18,6 @@ Um joguinho simples onde o usuário digita números para tentar acertar o valor 
 9. Garanta que a aplicação funciona corretamente.
 
 ## ESPECIFICAÇÕES E REQUISITOS;
+- Bibliotecas: random, tkinter;
 
 ## LOG
